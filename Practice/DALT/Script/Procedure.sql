9487cb160e74f8326f2b8c5f4400478bbab7d68e
Ngô Nhật Tân <56728366+NhatTanNgo@users.noreply.github.com>
2021-11-14 00:01:12 +0700

    StageToNDS

--- Practice/DALT/Script/Procedure.sql
+++ Practice/DALT/Script/Procedure.sql
@@ -38,6 +38,40 @@ SELECT @date = GETDATE(),
 END 
 
 go
---EXEC dbo.GET_CET 'Webtower9_HoaDon'
+
+USE NDS_Demo
+
+CREATE TABLE [dbo].[Receipt_NDS] (
+  [ReceiptID] int IDENTITY(1, 1) NOT NULL,
+  [ReceiptNK] varchar(4) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+  [CusID] int NOT NULL,
+  [StoreID] int NULL,
+  [SoldDay] datetime NULL,
+  [SourceID] int NULL,
+  [Status] int NULL,
+  [CreatedDay] datetime NULL,
+  [UpdatedDay] datetime NULL,
+
+PRIMARY KEY CLUSTERED 
+(
+	[ReceiptID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+ALTER TABLE dbo.Receipt_NDS
+ADD
+CONSTRAINT FK_Re_Cus
+FOREIGN KEY (CusID)
+REFERENCES dbo.Customer_NDS,
+
+CONSTRAINT FK_Re_Store
+FOREIGN KEY (StoreID)
+REFERENCES dbo.Store_NDS,
+
+CONSTRAINT FK_Re_Src
+FOREIGN KEY (SourceID)
+REFERENCES dbo.Source_NDS
 
 
+
+ 
