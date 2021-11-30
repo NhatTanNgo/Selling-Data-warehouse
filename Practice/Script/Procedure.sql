4a9bfc3eab8bef0a2cae42608aee4d97620a9df4
Ngô Nhật Tân <56728366+NhatTanNgo@users.noreply.github.com>
2021-11-30 09:47:33 +0700

    Update Staging

--- Practice/Script/Procedure.sql
+++ Practice/Script/Procedure.sql
@@ -1,4 +1,4 @@
-USE MetaData_Demo
+USE MetaData_DALT
 GO
 
 CREATE PROCEDURE GET_CET 
@@ -39,45 +39,111 @@ END
 
 go
 
-----
+
+
 USE DDS_DALT
-go
-CREATE PROCEDURE Check_Day 
-@day INT, @month INT, @year INT 
+GO
+CREATE FUNCTION CheckLeapYear
+(@year INT)
+RETURNS int
 AS
-DECLARE
-	@Idthang  INT  = 0 
 BEGIN
-	 SELECT @Idthang = IDThang  
-	 FROM dbo.ThangTrongNam 
-	 WHERE Thang = @month AND Nam = @year
-
-	 IF (@Idthang = 0)
-		BEGIN 
-			INSERT INTO dbo.ThangTrongNam
-			VALUES
-			(@month,@year)
-
-			SELECT @Idthang = IDThang  
-			FROM dbo.ThangTrongNam
-			WHERE Thang = @month AND Nam = @year
-		END 
+    IF(@year % 400 = 0) RETURN 1;
 	ELSE
 		BEGIN
-			IF NOT EXISTS (SELECT * FROM dbo.NgayTrongThang 
-						   WHERE IDThang = @Idthang AND Ngay = @day)
+			IF(@year % 4 = 0 AND @year % 100 <>0) RETURN 1;
+		END
+
+	RETURN 0
+END
+
+
+
+GO
+CREATE PROCEDURE CheckDay 
+@year INT
+AS
+BEGIN
+	DECLARE @leapyear INT =0,
+	@month INT = 1,
+	@day int = 1,
+	@february int = 28,
+	@monthID INT
+
+	IF NOT EXISTS (SELECT DISTINCT Nam FROM dbo.ThangTrongNam WHERE Nam = @year)
+	BEGIN
+		--Insert into table MonthInYear
+		WHILE @month <= 12
+		BEGIN
+			INSERT INTO dbo.ThangTrongNam
+			VALUES (@month, @year); 
+			SET @month = @month + 1;
+		END
+		SET @month = 1;
+
+
+		--Insert into table DayInMonth
+		WHILE @month <= 12
+		BEGIN
+			--Get MonthID
+			SELECT @monthID = IDThang FROM dbo.ThangTrongNam 
+			WHERE Thang = @month AND Nam = @year
+
+			--Month with 31 days
+			IF (@month IN (1,3,5,7,8,10,12))
+			BEGIN
+				WHILE @day <= 31
 				BEGIN
 					INSERT INTO dbo.NgayTrongThang
-					VALUES
-					(@Idthang, @day)   
+					VALUES (@monthID,@day)
+					SET @day = @day + 1
 				END
+			END
+			
+			--Month with 30 days
+			IF (@month IN (4,6,9,11))
+			BEGIN
+				WHILE @day <= 30
+				BEGIN
+					INSERT INTO dbo.NgayTrongThang
+					VALUES (@monthID,@day)
+					SET @day = @day + 1
+				END
+			END
+
+			--February
+			IF (@month = 2)
+			BEGIN
+				EXEC @leapyear = dbo.CheckLeapYear @year 
+				IF (@leapyear = 1) SET @february = 29
+				WHILE @day <= @february
+				BEGIN
+					INSERT INTO dbo.NgayTrongThang
+					VALUES (@monthID,@day)
+					SET @day = @day + 1
+				END	
+			END
+
+			SET @month = @month + 1
+			SET @day = 1
 		END
+	END
 END
 GO 
-
-
-
-
-
  
 
+
+--EXEC dbo.CheckDay 1900 -- int
+
+--SELECT * FROM dbo.ThangTrongNam
+--SELECT * FROM dbo.NgayTrongThang
+
+--DELETE dbo.ThangTrongNam
+--DBCC CHECKIDENT(ThangTrongNam,RESEED, 0)
+
+--DELETE dbo.NgayTrongThang
+--DBCC CHECKIDENT(NgayTrongThang, RESEED, 0)
+
+
+
+	 
